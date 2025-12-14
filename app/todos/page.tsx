39eed516dd1bb37ae4aa589834c9ebf37ba4fd5c
adenis <danchic95@gmail.com>
2025-12-14 23:31:42 +0500

    Fix server side prefetching

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,37 +1,46 @@
 import { CollapsibleCreateForm } from "@/app/todos/_components/CollapsibleCreateForm";
 import { TodoListSection } from "@/app/todos/_components/TodoListSection";
 import { BackButton } from "@/lib/components/BackButton";
-import { QueryClient } from "@tanstack/react-query";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { fetchTodoList } from "@/lib/todos/fetchTodoList";
+import { TODO_QUERY } from "@/lib/http/queries";
+import { TodoStatusFilter } from "@/lib/todos/types";
 
 export default async function TodosPage() {
   const queryClient = new QueryClient();
 
+  // TODO: get selected filters from URL in the future
   await queryClient.prefetchQuery({
-    queryKey: ["todos"],
-    queryFn: () => fetchTodoList(),
+    queryKey: [TODO_QUERY, TodoStatusFilter.ALL, ""],
+    queryFn: async () => await fetchTodoList(),
   });
 
   return (
-    <div className="flex w-full flex-col gap-8">
-      <div className="flex items-center gap-4">
-        <BackButton href="/" />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex w-full flex-col gap-8">
+        <div className="flex items-center gap-4">
+          <BackButton href="/" />
+        </div>
+
+        <header>
+          <h1 className="text-4xl font-bold leading-tight tracking-tight text-black dark:text-zinc-50">
+            My Todos
+          </h1>
+          <p className="mt-2 text-lg text-zinc-600 dark:text-zinc-400">
+            Create and manage your tasks
+          </p>
+        </header>
+
+        <div className="flex flex-col gap-12">
+          <CollapsibleCreateForm />
+
+          <TodoListSection />
+        </div>
       </div>
-
-      <header>
-        <h1 className="text-4xl font-bold leading-tight tracking-tight text-black dark:text-zinc-50">
-          My Todos
-        </h1>
-        <p className="mt-2 text-lg text-zinc-600 dark:text-zinc-400">
-          Create and manage your tasks
-        </p>
-      </header>
-
-      <div className="flex flex-col gap-12">
-        <CollapsibleCreateForm />
-
-        <TodoListSection />
-      </div>
-    </div>
+    </HydrationBoundary>
   );
 }
